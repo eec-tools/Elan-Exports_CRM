e44505e25476259e497c82c1ed22a751459e112c
Harsh Patel <hp1659414@gmail.com>
2026-03-23 17:14:24 +0530

    cors error fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -52,8 +52,7 @@ app.use((req, res, next) => {
 // CORS configuration
 const hardcodedOrigins = [
   "http://localhost:5173",
-  "http://elan-exports-frontend.s3-website.ap-south-1.amazonaws.com",
-  "https://d2j8d56nigd3ee.cloudfront.net"
+  "http://elan-exports-s3.s3-website.ap-south-1.amazonaws.com",
 ];
 
 const envOrigins = process.env.FRONTEND_URL
